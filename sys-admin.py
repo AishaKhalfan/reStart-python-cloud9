06ab7af25e27d0859370f941cc84742fe5ea9241
AishaKhalfan <aishakhalfan45@gmail.com>
2023-10-29 19:50:52 +0000

    Sys ADMIN

--- sys-admin.py
+++ sys-admin.py
@@ -1,3 +1,17 @@
 # Exercise 1: Using os.system
-import os
-os.system("ls")
+# import os
+# os.system("ls")
+import subprocess
+subprocess.run(["ls","-l","README.md"])
+
+# Exercise 5: Retrieving system information
+command="uname"
+commandArgument="-a"
+print(f'Gathering system information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
+
+# Exercise 6: Retrieving information about disk space
+command="ps"
+commandArgument="-x"
+print(f'Gathering active process information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
